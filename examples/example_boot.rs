64a43bfe0166f73ce19a2a1c683abd3dcada2a5a
jacquesmouton <jmouton19@gmail.com>
2023-04-02 02:19:56 +0200

    more abstraction

--- examples/example_boot.rs
+++ examples/example_boot.rs
@@ -4,95 +4,17 @@ use space_net::utils::*;
 use std::collections::HashMap;
 
 fn main() {
-    let mut boot_node = Node::new(Config::default());
-    println!("boot node online..... {:?}", boot_node.zid);
 
-    let counter_subscriber = boot_node
-        .session
-        .declare_subscriber("counter/*")
-        .reliable()
-        .res()
-        .unwrap();
-    let boot_subscriber = boot_node
-        .session
-        .declare_subscriber("node/boot/*")
-        .reliable()
-        .res()
-        .unwrap();
-    let node_subscriber = boot_node
-        .session
-        .declare_subscriber(format!("node/{}/*", boot_node.zid))
-        .reliable()
-        .res()
-        .unwrap();
+    let mut boot_server=BootNode::new_with_node(Node::new(Config::default()));
+    //let mut boot_node=&boot_server.node.unwrap();
+    //let mut boot_node = Node::new(Config::default());
 
-    //set boot node point
-    boot_node.site = (50., 50.);
-    //add boot node to cluster
-    let mut cluster = OrderedMapPairs::new();
-    cluster.insert(boot_node.zid.to_string(), boot_node.site);
+    println!("boot node online..... {:?}", boot_server.node.zid);
 
-    //draw initial voronoi
-    let diagram = Voronoi::new(boot_node.site, &boot_node.neighbours);
-    let polygon = diagram.diagram.cells()[0]
-        .points()
-        .iter()
-        .map(|x| (x.x, x.y))
-        .collect();
-    let mut polygon_list = OrderedMapPolygon::new();
-    polygon_list.insert(boot_node.zid.to_string(), polygon);
-    draw_voronoi_full(&cluster, &polygon_list, "initial");
-    let mut draw_count = 1;
-
-    let mut correct_polygon_list = OrderedMapPolygon::new();
     loop {
+        boot_server.run();
         // Handle messages in the queue
-        if let Ok(sample) = boot_subscriber.try_recv() {
-            let mut expected_counter = -1;
-            let mut counter = 0;
 
-            boot_callback(sample, &mut boot_node, &mut polygon_list, &mut cluster);
-            // Process the message here
-
-            while expected_counter != counter {
-                while let Ok(sample) = counter_subscriber.try_recv() {
-                    counter_callback(
-                        sample,
-                        &mut expected_counter,
-                        &mut counter,
-                        &mut polygon_list,
-                    );
-                    // Process the message here
-                }
-                while let Ok(sample) = node_subscriber.try_recv() {
-                    node_callback(sample, &mut boot_node);
-                    // Process the message here
-                }
-            }
-            //redraw distributed voronoi
-            draw_voronoi_full(
-                &cluster,
-                &polygon_list,
-                format!("voronoi{}", draw_count).as_str(),
-            );
-
-            //draw correct voronoi
-            let mut temp_cluster = cluster.clone();
-            temp_cluster.remove(boot_node.zid.as_str());
-            let hash_map: HashMap<String, (f64, f64)> = temp_cluster.into_iter().collect();
-            let temphash = SiteIdList { sites: hash_map };
-            let diagram = Voronoi::new(*cluster.values().next().unwrap(), &temphash);
-            for (i, cell) in diagram.diagram.cells().iter().enumerate() {
-                let polygon = cell.points().iter().map(|x| (x.x, x.y)).collect();
-                correct_polygon_list.insert(format!("{i}").to_string(), polygon);
-            }
-            draw_voronoi_full(
-                &cluster,
-                &correct_polygon_list,
-                format!("confirm{}", draw_count).as_str(),
-            );
-            draw_count += 1;
-        }
         // Perform other tasks here
         // Wait for some time before starting to handle messages again
         //sleep(Duration::from_secs(1));
--- src/node.rs
+++ src/node.rs
@@ -5,8 +5,9 @@ use std::sync::Arc;
 use serde_json::json;
 pub use zenoh::prelude::sync::*;
 use zenoh::subscriber::Subscriber;
-use crate::handlers::node_callback;
+use crate::handlers::{boot_callback, counter_callback, node_callback};
 use crate::message::NewNodeRequest;
+use crate::utils::{draw_voronoi_full, Voronoi};
 
 
 pub type OrderedMapPairs = LinkedHashMap<String, (f64, f64)>;
@@ -26,15 +27,22 @@ pub struct Node<'a> {
     pub received_counter: i32,
     pub expected_counter: i32,
     pub running:bool,
-    pub sub:Subscriber<'a,flume::Receiver<Sample>>,
+    sub:Subscriber<'a,flume::Receiver<Sample>>,
 }
 
 // #[derive(Clone)]
-// pub struct BootNode{
-//     pub node:Node,
-//     pub received_counter:i32,
-//     pub expected_counter:i32,
-// }
+pub struct BootNode<'a>{
+    pub node: Node<'a>,
+    pub received_counter:i32,
+    pub expected_counter:i32,
+    pub running:bool,
+    sub_boot:Subscriber<'a,flume::Receiver<Sample>>,
+    sub_counter:Subscriber<'a,flume::Receiver<Sample>>,
+    pub cluster: OrderedMapPairs,
+    pub polygon_list: OrderedMapPolygon,
+    pub correct_polygon_list: OrderedMapPolygon,
+    pub draw_count: i32,
+}
 
 impl Node<'_> {
     pub fn new(config: Config) -> Self {
@@ -71,15 +79,96 @@ impl Node<'_> {
     }
 }
 
-// impl BootNode{
-//     pub fn new(config:Config)-> Self{
-//         Self{
-//             node:Node::new(config),
-//             received_counter:0,
-//             expected_counter:-1,
-//         }
-//     }
-// }
+impl <'a> BootNode<'a>{
+    pub fn new_with_node(mut node: Node<'a>) -> Self {
+        let counter_subscriber = node
+            .session
+            .declare_subscriber("counter/*")
+            .reliable()
+            .res()
+            .unwrap();
+        let boot_subscriber = node
+            .session
+            .declare_subscriber("node/boot/*")
+            .reliable()
+            .res()
+            .unwrap();
+        node.site = (50., 50.);
+        let mut cluster=OrderedMapPairs::new();
+        cluster.insert(node.zid.to_string(), node.site);
+
+        let diagram = Voronoi::new(node.site, &node.neighbours);
+        let polygon = diagram.diagram.cells()[0]
+            .points()
+            .iter()
+            .map(|x| (x.x, x.y))
+            .collect();
+
+        let mut polygon_list = OrderedMapPolygon::new();
+        polygon_list.insert(node.zid.to_string(), polygon);
+        draw_voronoi_full(&cluster, &polygon_list, "initial");
+        Self { node, received_counter: 0, expected_counter: -1, running: true, sub_boot: boot_subscriber, sub_counter: counter_subscriber, cluster, polygon_list, correct_polygon_list:OrderedMapPolygon::new(), draw_count: 1 }
+    }
+
+    // pub fn new_without_node() -> Self {
+    //     Self { node: None, received_counter: 0, expected_counter: -1, running: true }
+    // }
+
+    pub fn run(&mut self){
+        let mut boot_node=&mut self.node;
+
+        if let Ok(sample) = self.sub_boot.try_recv() {
+            self.expected_counter = -1;
+            self.received_counter = 0;
+
+            boot_callback(sample, &mut boot_node, &mut self.polygon_list, &mut self.cluster);
+            // Process the message here
+
+            while self.expected_counter != self.received_counter {
+                while let Ok(sample) = self.sub_counter.try_recv() {
+                    counter_callback(
+                        sample,
+                        &mut self.expected_counter,
+                        &mut self.received_counter,
+                        &mut self.polygon_list,
+                    );
+                    // Process the message here
+                }
+                boot_node.run();
+            }
+            //redraw distributed voronoi
+            draw_voronoi_full(
+                &self.cluster,
+                &self.polygon_list,
+                format!("voronoi{}", self.draw_count).as_str(),
+            );
+
+            //draw correct voronoi
+            let mut temp_cluster = self.cluster.clone();
+            temp_cluster.remove(boot_node.zid.as_str());
+            let hash_map: HashMap<String, (f64, f64)> = temp_cluster.into_iter().collect();
+            let temphash = SiteIdList { sites: hash_map };
+            let diagram = Voronoi::new(*self.cluster.values().next().unwrap(), &temphash);
+            for (i, cell) in diagram.diagram.cells().iter().enumerate() {
+                let polygon = cell.points().iter().map(|x| (x.x, x.y)).collect();
+                self.correct_polygon_list.insert(format!("{i}").to_string(), polygon);
+            }
+            draw_voronoi_full(
+                &self.cluster,
+                &self.correct_polygon_list,
+                format!("confirm{}", self.draw_count).as_str(),
+            );
+            self.draw_count += 1;
+        }
+
+    }
+}
+
+
+
+
+
+
 
 impl SiteIdList {
     pub fn new() -> SiteIdList {
