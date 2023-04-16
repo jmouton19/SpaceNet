27ee12ac6c37a8dccecbf58a15ea4e372cca30c9
jacquesmouton <jmouton19@gmail.com>
2023-04-16 03:28:19 +0200

    fix doc examples

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,7 @@
 //!
 //! fn main() {
 //!     let mut node = Node::new(Config::default(), "network_1").leave_on_pressed('q');
-//!     println!("node online..... {:?}", boot_server.get_zid());
+//!     println!("node online..... {:?}", node.get_zid());
 //!     loop {
 //!         if !node.is_running() {
 //!            break;
